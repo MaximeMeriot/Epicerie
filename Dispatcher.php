e05c092fd5eaab78f634e987f6d82bd5fcdfd82a
MaximeMeriot <51957021+MaximeMeriot@users.noreply.github.com>
2019-07-16 11:21:48 +0200

    h

--- Dispatcher.php
+++ Dispatcher.php
@@ -9,10 +9,10 @@ class Dispatcher{
 
     public function dispatch(){
 
-        $controller=(isset($_GET['controller']))?$_GET['controller']:"Home";
+        $controller=(isset($_GET['controller']))?$_GET['controller']:"Connect";
         $controller=$controller."Controller";
 
-        $action=(isset($_GET['action']))?$_GET['action']:"show";
+        $action=(isset($_GET['action']))?$_GET['action']:"login";
         $action=$action."Action";
 
         $my_controller=new $controller();
