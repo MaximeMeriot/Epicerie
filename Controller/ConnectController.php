842aaafad0b3bf96f8ef613bdf4278f7c3a28ec6
MaximeMeriot <51957021+MaximeMeriot@users.noreply.github.com>
2019-07-16 09:37:25 +0200

    fe

--- Controller/ConnectController.php
+++ Controller/ConnectController.php
@@ -1,17 +1,5 @@
 <?php
-class Controller extends  {
-
-    private $view;
-    private $model;
-
-
-    public function __construct(){
-        /*
-        * On instancie notre View/Model
-        */ 
-        $this->view = new View();
-        $this->model = new Model();
-    }
+class ConnectController extends  {
 
 
 }
