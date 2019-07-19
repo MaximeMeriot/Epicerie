5f352db2b374550b15480c257f59a1e140f4c2db
matthieucaballero <51957195+matthieucaballero@users.noreply.github.com>
2019-07-19 12:19:35 +0200

    Bienvenue

--- View/MotherView.php
+++ View/MotherView.php
@@ -8,6 +8,12 @@ abstract class MotherView{
     function __construct(){
         $this->page=$this->searchHtml("html/header.html");
         $this->page.=$this->searchHtml("html/nav.html");
+
+        if(isset($_SESSION['nom'])&&isset($_SESSION['prenom'])&&isset($_SESSION['societe'])){
+
+            $this->page = str_replace("{{utilisateur}}",$_SESSION['nom'].' '.$_SESSION['prenom'].' de la société '.$_SESSION['societe'],$this->page);
+        }
+
     }
 //--------------------------------------------------------------------------------------------------------------------------------------
 
