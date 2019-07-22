be7ed18572962ef49a63b004ddf53ae87fc1fa14
matthieucaballero <51957195+matthieucaballero@users.noreply.github.com>
2019-07-22 16:26:32 +0200

    azdacs

--- Controller/ValidPanierController.php
+++ Controller/ValidPanierController.php
@@ -20,13 +20,7 @@ class ValidPanierController extends MotherController
         $this->view->displayValid();
         $this->model->sendMail();
 
-        $id=array('2','3');
-        $qte=array('10','5');
-        $panier=array(
-            "id"=> $id,"qte"=>$qte
-        );
-
-        $this->model->validCommande($panier, $_SESSION['id']);
+        $this->model->validCommande($_SESSION['cart'], $_SESSION['id']);
     }
 
 }
