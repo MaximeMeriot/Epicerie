08dc8cec7d89b50fa57f0e606351ec8acc5b1082
marjolaineLacan <52748495+marjolaineLacan@users.noreply.github.com>
2019-07-23 19:42:37 +0200

    Add files via upload

--- Epicerie/Model/ListModel.php
+++ Epicerie/Model/ListModel.php
@@ -1,32 +1,30 @@
 <?php
-
 class ListModel extends MotherModel
 {
-
     function getList()
     {
+        $requete = "SELECT * FROM produit";
+        $pre = $this->connexion->prepare($requete);
+        $pre->execute();
+        $list = $pre->fetchAll(PDO::FETCH_ASSOC);
 
-        $requete = "SELECT * FROM Produit";
-        $result = $this -> connexion -> query($requete);
-        $list = array();
-        if ($result){
-            $list = $result -> fetchAll(PDO::FETCH_ASSOC);
-        }
         return $list;
-    
     }
+    public function panierlist()
+    {
 
-    function addCompteur($idProduit)
-    {  
-        $compteur = 'produit' . $idProduit;
-    
-        if (isset($_SESSION["$compteur"])){
-            $_SESSION["$compteur"] ++;
-        
-        } else {
-            $_SESSION["$compteur"] = 1;
+        // session_destroy();
+        if (isset($_GET["id"])) {
+            # code...
+            if (isset($_SESSION["cart"])) {
 
+                if (!in_array($_GET["id"], $_SESSION["cart"])) {
+                    # code...
+                    array_push($_SESSION["cart"], $_GET["id"]);
+                }
+            } else {
+                $_SESSION["cart"][0] = $_GET["id"];
+            }
         }
     }
-
 }
