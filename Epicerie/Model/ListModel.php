20d1ad1c9dd04968dca0e63ff66cb22070c350b3
marjolaineLacan <52748495+marjolaineLacan@users.noreply.github.com>
2019-07-23 19:31:43 +0200

    panier boutons + et -

--- Epicerie/Model/ListModel.php
+++ Epicerie/Model/ListModel.php
@@ -1,37 +1,42 @@
 <?php
-
 class ListModel extends MotherModel
 {
-
     function getList()
     {
         $requete = "SELECT * FROM produit";
         $pre = $this ->connexion -> prepare($requete);
         $pre->execute();
+        $list = array();
         $list = $pre-> fetchAll(PDO::FETCH_ASSOC);
         
         return $list;
     }
 
-    public function panierlist()
+    
+    public function addCompteur($idProduit)
     {
-       
-        // session_destroy();
-        if (isset($_GET["id"])) {
-            # code...
-            if (isset($_SESSION["cart"])) {
-                
-                if (!in_array($_GET["id"],$_SESSION["cart"])) {
-                    # code...
-                    array_push($_SESSION["cart"],$_GET["id"]);
-                }
-                
-                
-            }else {
-                $_SESSION["cart"][0] = $_GET["id"];
-            }
-        }  
-            
+
+        $compteur = 'produit' . $idProduit;
+    
+        if (isset($_SESSION["$compteur"])){
+            $_SESSION["$compteur"] ++;
+        
+        } else {
+            $_SESSION["$compteur"] = 1;
+
+        }
+      
+    }
+
+    public function removeCompteur($idProduit)
+    {
+
+        $compteur = 'produit' . $idProduit;
+    
+        if (isset($_SESSION["$compteur"])){
+            $_SESSION["$compteur"] --;
+        }
+      
     }
    
 
