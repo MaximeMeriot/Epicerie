085e684a3122ec417e4e0ec427ccff83c3f1c008
marjolaineLacan <52748495+marjolaineLacan@users.noreply.github.com>
2019-07-24 09:40:29 +0200

    Add files via upload

--- Model/ValidPanierModel.php
+++ Model/ValidPanierModel.php
@@ -5,7 +5,6 @@ use PHPMailer\PHPMailer\Exception;
 
 require 'PHPMailer-master/src/PHPMailer.php';
 require 'PHPMailer-master/src/Exception.php';
-
 class ValidPanierModel extends MotherModel
 {
     public function sendMail()
@@ -16,9 +15,7 @@ class ValidPanierModel extends MotherModel
         $nom = $_SESSION['nom'];
         $societe = $_SESSION['societe'];
         $email = $_SESSION['email'];
-
-        $message = "Bonjour ". " " . $prenom . " " .  $nom ." de l'entreprise ".  $societe .", votre commande est enregistrée";
-
+        $message = "Bonjour, votre commande est enregistrée";
 
         try {
             $mail->From = "marjo@lacan.me";
@@ -29,125 +26,116 @@ class ValidPanierModel extends MotherModel
             $mail->Body = $message;
             $mail->AddAddress($email, $nom, $prenom);
             $res = $mail->Send();
-
             // echo "Résultat de l'envoi : ";
             // var_dump($res);
             // var_dump($email);
-
         } catch (Exception $e) {
             echo 'Message non envoyé';
             echo 'Erreur: ' . $mail->ErrorInfo;
         }
     }
-//------------------------------------------------------------------------------------------------------------------------
-    public function validCommande($panier, $idClient){
-
-       $numCommande=$this->getNextNum();
-       $date=date("y.m.d");
-
-
-       //--------Entete commande-------------------------------------------------------------------
-       $requete = $this->connexion->prepare("INSERT INTO entete_commande
+    //------------------------------------------------------------------------------------------------------------------------
+    public function validCommande($panier, $idClient)
+    {
+        $numCommande = $this->getNextNum();
+        $date = date("y.m.d");
+        //--------Entete commande-------------------------------------------------------------------
+        $requete = $this->connexion->prepare("INSERT INTO entete_commande
        VALUES (:num_commande,:date_commande,:id_client)");
-   
-       $requete->bindParam(':num_commande',$numCommande);
-       $requete->bindParam(':date_commande',$date);
-       $requete->bindParam(':id_client',$idClient);
-   
-       $requete->execute();
 
+        $requete->bindParam(':num_commande', $numCommande);
+        $requete->bindParam(':date_commande', $date);
+        $requete->bindParam(':id_client', $idClient);
+
+        $requete->execute();
         //--------Detail commande-------------------------------------------------------------------
-
-        foreach($panier as $value){
-
+        foreach ($panier as $value) {
             var_dump($panier);
-            $etat_commande="Terminée";
-
-
+            $etat_commande = "Terminée";
             $requete = $this->connexion->prepare("INSERT INTO detail_commande
             VALUES (NULL,:etat_commande,:qte_produit_commande, :num_commande, :id_produit)");
 
-        
-            $requete->bindParam(':etat_commande',$etat_commande);
-            $requete->bindParam(':qte_produit_commande',$value['qte']);
-            $requete->bindParam(':num_commande',$numCommande);
-            $requete->bindParam(':id_produit',$idClient);
-        
+            $requete->bindParam(':etat_commande', $etat_commande);
+            $requete->bindParam(':qte_produit_commande', $value['qte']);
+            $requete->bindParam(':num_commande', $numCommande);
+            $requete->bindParam(':id_produit', $idClient);
 
-            $requete->execute();                 
-
+            $requete->execute();
         }
-
     }
-//------------------------------------------------------------------------------------------------------------------------
-    public function getNextNum(){    
-       
-       $requete = $this->connexion->prepare('SELECT MAX(num_commande) FROM entete_commande');
-       $requete->execute();
-       $num = $requete->fetch(PDO::FETCH_ASSOC);
-       
-       return ($num['MAX(num_commande)']+1);
+    //------------------------------------------------------------------------------------------------------------------------
+    public function getNextNum()
+    {
 
+        $requete = $this->connexion->prepare('SELECT MAX(num_commande) FROM entete_commande');
+        $requete->execute();
+        $num = $requete->fetch(PDO::FETCH_ASSOC);
+
+        return ($num['MAX(num_commande)'] + 1);
     }
-//------------------------------------------------------------------------------------------------------------------------
-       
-// Gestion du stock           
-public function checkCart() {
-        foreach($_SESSION['cart'] as $element){
+    //------------------------------------------------------------------------------------------------------------------------
+
+    // Gestion du stock           
+    public function checkCart()
+    {
+        $retour = true;
+
+        foreach ($_SESSION as $key => $value) {
+
+            if (strpos($key, 'produit') !== false) {
+                echo '<pre>';
+                var_dump($key);
+                var_dump($value);
+
+                $id = substr($key, 7);
+                if ($this->verifQte($id, $value)) {
+                    $this->updateStock($id, $value);
+                } else{
+                    $retour=false;
+                }
+            }
+        }
+        return $retour;
+    }
+
+    public function verifQte($id_produit, $quantite)
+    {
+
             $requete = $this->connexion->prepare("SELECT qte_stock FROM produit WHERE id_produit = :id_produit");
-        
-            $requete->bindParam(':id_produit', $element);
-            // $resultat = $this->connexion->query($requete);
+
+            $requete->bindParam(':id_produit', $id_produit);
             $requete->execute();
             $resultat = $requete->fetch(PDO::FETCH_ASSOC);
- 
-            if ($resultat['qte_stock']<1) {
+
+// var_dump($resultat);
+
+            if ($resultat['qte_stock'] < $quantite) {
                 return false;
+            } else{
+                return true;
             }
-
-//             echo '<pre>';
-//             var_dump($resultat);
-
-        }
-        foreach($_SESSION['cart'] as $element){
-            $this ->updateStock($element, 1);
-        }
-        return true;
     }
 
-   
-     public function updateStock($id_produit, $qty)
-     {
-         $requete = $this->connexion->prepare("UPDATE produit SET qte_stock = qte_stock-:qty WHERE id_produit = :id_produit");
-        
-         $requete->bindParam(':id_produit', $id_produit);
-         $requete->bindParam(':qty', $qty);
+    public function updateStock($id_produit, $qty)
+    {
+        $requete = $this->connexion->prepare("UPDATE produit SET qte_stock = qte_stock-:qty WHERE id_produit = :id_produit");
 
-         $resultat = $requete->execute();
+        $requete->bindParam(':id_produit', $id_produit);
+        $requete->bindParam(':qty', $qty);
+        $resultat = $requete->execute();
+        return $resultat;
+    }
 
-         return $resultat;
-     } 
-//------------------------------------------------------------------------------------------------------------------------
-public function jsonFile(){
+    public function jsonFile()
+    {
+        $jsonTab["date"] = date("Y-m-d H:i:s");
+        $jsonTab["numero_de_commande"] = "EOL-5000";
+        $jsonTab["items"] = [
+            "tomates" => "3",
+            "concombre" => "1"
+        ];
 
-    $jsonTab["date"]=date("Y-m-d H:i:s");
-    $jsonTab["numero_de_commande"]="EOL-5000";
-    $jsonTab["items"]=[
-            "Tomates"=>"3",
-            "Cornichons"=>"3546",
-            "Pâté"=>"1"
-    ];
-
-    $jsonFile=json_encode($jsonTab);
-
-    file_put_contents("json/jsonTest.json",$jsonFile);
-
+        $jsonFile = json_encode($jsonTab);
+        file_put_contents("json/jsonTest.json", $jsonFile);
+    }
 }
-//------------------------------------------------------------------------------------------------------------------------
-
-
-
-
-}
-
-
